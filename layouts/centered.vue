6599e4569ab6e9a7bf68a78581be451865d7e002
Jeremy <jeremy.mees@appeel.io>
2022-10-28 14:54:20 +0200

    fix(centered layout): you can now close centered layout with routing back to the home page

--- layouts/centered.vue
+++ layouts/centered.vue
@@ -1,7 +1,14 @@
+<script setup>
+import Remove from '~/assets/icons/remove.svg'
+</script>
+
 <template>
   <div class="w-screen min-h-screen">
     <div class="container absolute w-full max-w-xl -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
-      <div class="bg-tracker tracker-shadow-pulse px-2 py-6 sm:p-8 rounded">
+      <div class="bg-tracker tracker-shadow-pulse px-2 py-6 sm:p-8 rounded relative">
+        <NuxtLink to="/">
+          <Remove class="w-5 h-5 text-danger cursor-pointer absolute right-4 top-4" />
+        </NuxtLink>
         <slot />
       </div>
     </div>
