d1fddbc17c7960f1faef68d398372c1f586c56f2
Jeremy <jeremy.mees@appeel.io>
2022-12-18 11:17:17 +0100

    feat(lang): lang cookie now gets stored in the cookies

--- app.vue
+++ app.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { onBeforeMount } from 'vue'
 import { useProfileStore } from '@/store/profile'
 
 const appName = 'DnD tracker'
--- components/atoms/LangSwitcher.vue
+++ components/atoms/LangSwitcher.vue
@@ -2,6 +2,18 @@
 import { useI18n } from 'vue-i18n'
 
 const { locale, availableLocales } = useI18n({ useScope: 'global' })
+const cookieLang = useCookie('lang')
+const localeLang = computed(() => cookieLang.value || locale.value)
+
+// set the language when mounted if there's a cookie for it
+onMounted(() => {
+  if (cookieLang.value) locale.value = cookieLang.value
+})
+
+function setLang(lang) {
+  cookieLang.value = lang
+  locale.value = lang
+}
 </script>
 
 <template>
@@ -10,8 +22,8 @@ const { locale, availableLocales } = useI18n({ useScope: 'global' })
       <p v-if="index > 0 && index < availableLocales.length">|</p>
       <p
         class="text-slate-300 hover:text-white duration-200 ease-in-out cursor-pointer"
-        :class="{ '!text-primary': locale === lang }"
-        @click="locale = lang"
+        :class="{ '!text-primary': localeLang === lang }"
+        @click="setLang(lang)"
       >
         {{ lang }}
       </p>
