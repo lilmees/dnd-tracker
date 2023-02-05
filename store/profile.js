59c7177f6c0a911ae6d71a28c11459b8a1481f28
Jeremy <jeremy.mees@appeel.io>
2023-02-05 14:21:01 +0100

    feat(supabase): updated the package from 0.1.27 to 0.30

--- store/profile.js
+++ store/profile.js
@@ -15,10 +15,9 @@ export const useProfileStore = defineStore('useProfileStore', {
         this.error = null
         this.loading = true
         const { data, error } = await supabase.from('profiles').select('*').eq('id', user.value.id).single()
-        if (data) this.data = data
         if (error) this.error = error
+        if (data) this.data = data
       } catch (error) {
-        console.error(error)
         this.error = error
       } finally {
         this.loading = false
