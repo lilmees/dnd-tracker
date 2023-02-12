5e48b58b0127ac4924ead3ecc250917ea66ee3b6
Jeremy <jeremy.mees@appeel.io>
2023-02-12 21:17:40 +0100

    fix(footer): foote only visible on bottom of page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col min-h-screen max-h-screen">
+  <div class="flex flex-col min-h-screen">
     <Navbar />
     <main class="max-w-[1800px] mx-auto w-full px-4 sm:px-8 md:px-12 lg:px-16 no-scrollbar overflow-y-auto grow">
       <slot />
--- layouts/wide.vue
+++ layouts/wide.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col min-h-screen max-h-screen">
+  <div class="flex flex-col min-h-screen">
     <Navbar />
     <main class="max-w-[3000px] mx-auto w-full px-4 no-scrollbar overflow-y-auto grow flex flex-col">
       <slot />
