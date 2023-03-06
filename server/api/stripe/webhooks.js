97b9c24199452e9a8b1b29bc3868261bb6cf24b5
Jeremy <jeremy.mees@appeel.io>
2023-03-06 20:03:39 +0100

    feat(stripe): webhook

--- server/api/stripe/webhooks.js
+++ server/api/stripe/webhooks.js
@@ -1,5 +1,4 @@
 import { serverSupabaseClient } from '#supabase/server'
-import Stripe from 'stripe'
 
 const config = useRuntimeConfig()
 
@@ -10,7 +9,7 @@ export default defineEventHandler(async event => {
   const subscription = body.data.object
   const typeId = subscription.items.data[0].plan.id
   let productName
-  const stripeMeta = {
+  const stripeData = {
     stripe_last_event: body.created,
     stripe_status: subscription.status,
     stripe_trail_ends_at: subscription.trail_end,
@@ -21,32 +20,31 @@ export default defineEventHandler(async event => {
   if (typeId === config.public.stripeMediorMonthly || typeId === config.public.stripeMediorYearly) productName = 'medior'
   else if (typeId === config.public.stripeProMonthly || typeId === config.public.stripeProYearly) productName = 'pro'
 
-  const profile = await client.from('profiles').select('stripe_last_event').eq('stripe_id', subscription.customer).single()
-
-  if (profile.stripe_last_event > body.created) return `Did not handle ${body.event} because it was an old event`
+  const { data } = await client.from('profiles').select('stripe_last_event').eq('stripe_id', subscription.customer).single()
+  if (data?.stripe_last_event > body.created) return `Did not handle ${body.type} because it was an old event`
 
   if (
     body.type === 'customer.subscription.created'
     || body.type === 'customer.subscription.resumed'
     || body.type === 'customer.subscription.updated'
   ) {
-    await client.from('profiles').update({
-      ...stripeMeta,
+    stripeData = {
+      ...stripeData,
       paid_subscription_active: true,
       subscription_id: subscription.id,
       subscription_type: productName,
-
-    }).eq('stripe_id', subscription.customer)
+    }
   }
   else if (body.type === 'customer.subscription.deleted' || body.type === 'customer.subscription.paused') {
-    await client.from('profiles').update({
-      ...stripeMeta,
+    stripeData = {
+      ...stripeData,
       paid_subscription_active: false,
       subscription_id: null,
       subscription_type: 'free',
-    }).eq('stripe_id', subscription.customer)
+    }
   }
 
+  await client.from('profiles').update(stripeData).eq('stripe_id', subscription.customer)
 
-  return `handled ${body.event}`
+  return `handled ${body.type}`
 })
