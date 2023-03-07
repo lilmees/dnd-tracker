2efaeaab45adf9ad80bda2c26b843ba14553bdf4
Jeremy <jeremy.mees@appeel.io>
2023-03-07 17:24:04 +0100

    fix(encounter): only show campaign buttons when it is a campaign encounter

--- components/organisms/EncounterOptions.vue
+++ components/organisms/EncounterOptions.vue
@@ -8,8 +8,10 @@ defineProps({
 <template>
   <div class="flex flex-wrap justify-end gap-2">
     <AddInitiativeMonster :encounter="encounter" :sandbox="sandbox" />
-    <AddInitiativeCampaignMonster :encounter="encounter" :sandbox="sandbox" />
-    <AddInitiativeCampaignPlayer :encounter="encounter" :sandbox="sandbox" />
+    <template v-if="encounter.campaign">
+      <AddInitiativeCampaignMonster :encounter="encounter" :sandbox="sandbox" />
+      <AddInitiativeCampaignPlayer :encounter="encounter" :sandbox="sandbox" />
+    </template>
     <AddInitiativeModal :encounter="encounter" :sandbox="sandbox" />
   </div>
 </template>
