48863cbbf86a44a54ef22b212036623ce1719407
Jeremy <jeremy.mees@appeel.io>
2023-03-26 12:18:12 +0200

    feat(cookie banner): added cookie icon to cookie banner

--- components/atoms/CookieBanner.vue
+++ components/atoms/CookieBanner.vue
@@ -1,14 +1,17 @@
 <script setup>
+import Cookie from '@/assets/icons/cookie.svg'
+
 const acceptedCookie = useCookie('accepted')
 const localePath = useLocalePath()
 </script>
-
+<!-- v-if="!acceptedCookie" -->
 <template>
-  <div v-if="!acceptedCookie">
+  <div >
     <div class="bg-black/50 inset-0 fixed" />
     <div
       class="fixed z-50 flex flex-col items-center justify-center p-6 bg-primary rounded-md gap-y-10 bottom-4 left-4 right-4 md:flex-row md:justify-between md:px-10 md:py-8 md:bottom-10 md:left-10 md:right-10"
     >
+      <Cookie class="w-12"/>
       <div class="flex flex-col items-center text-center gap-y-6 md:flex-row md:inline-flex md:gap-x-7">
         <h2>
           {{ $t('cookie.text') }}
