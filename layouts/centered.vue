aa9b01d651a2ec586c28911d804fc9d7cf296f6a
Jeremy <jeremy.mees@appeel.io>
2023-05-01 19:03:24 +0200

    DND-115 layouts typescript

--- layouts/centered.vue
+++ layouts/centered.vue
@@ -1,12 +1,19 @@
-<script setup>
+<script setup lang="ts">
 const localePath = useLocalePath()
 </script>
 
 <template>
   <div class="w-screen min-h-screen relative">
-    <div class="absolute inset-0 bg-black/80 cursor-pointer animate-pulse" @click="navigateTo(localePath('/'))" />
-    <div class="container absolute w-full max-w-2xl -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
-      <div class="bg-tracker px-2 py-6 sm:p-8 rounded-xl relative tracker-shadow">
+    <div
+      class="absolute inset-0 bg-black/80 cursor-pointer animate-pulse"
+      @click="navigateTo(localePath('/'))"
+    />
+    <div
+      class="container absolute w-full max-w-2xl -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2"
+    >
+      <div
+        class="bg-tracker px-2 py-6 sm:p-8 rounded-xl relative tracker-shadow"
+      >
         <NuxtLink :to="localePath('/')" class="group">
           <Icon
             name="ic:round-clear"
