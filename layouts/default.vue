a7449409cba11ca0bdfb28f56cb13ee52a4affb9
jeremymees <jeremymees123@gmail.com>
2023-08-11 08:48:28 +0200

    DND-231 dont show footer and navigation when on default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+const showNav = useState<boolean>('showNavigation', () => true)
+
+onBeforeMount(() => { showNav.value = true })
+</script>
+
 <template>
   <main class="mt-[150px] mb-20 dnd-container no-scrollbar overflow-y-auto grow">
     <slot />
