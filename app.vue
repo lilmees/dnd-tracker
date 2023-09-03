e1a3e8b31c4fd7f9688e0055aa6e1bba290ce25f
jeremymees <jeremymees123@gmail.com>
2023-09-03 19:38:40 +0200

    DND-146 pwa banner test

--- app.vue
+++ app.vue
@@ -33,7 +33,6 @@ onBeforeMount(() => profile.fetch())
     <VitePwaManifest />
     <Toasts />
     <DiceNotification />
-    <PwaInstallPrompt />
 
     <div class="flex flex-col min-h-screen">
       <Navbar class="fixed z-10 left-0 right-0 backdrop-blur" />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -65,31 +65,27 @@ export default defineNuxtConfig({
       name: 'DnD-Tracker',
       short_name: 'Tracker',
       description: 'Effortless Encounter Management',
-      background_color: '#2A303C',
       theme_color: '#21252E',
-      start_url: 'https://dnd-tracker.com',
+      start_url: '/',
       lang: 'en',
       icons: [
         {
-          src: 'pwa-192.png',
+          src: '/pwa-192.png',
           sizes: '192x192',
           type: 'image/png'
         },
         {
-          src: 'pwa-512.png',
+          src: '/pwa-512.png',
           sizes: '512x512',
           type: 'image/png'
         },
         {
-          src: 'pwa-512.png',
+          src: '/pwa-512.png',
           sizes: '512x512',
           type: 'image/png',
-          purpose: 'any maskable'
+          purpose: 'maskable'
         }
       ]
-    },
-    client: {
-      installPrompt: true
     }
   },
   vite: {
