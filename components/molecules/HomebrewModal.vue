a6a922d48e6d3ed34afb4dfec133091bcee0c0b4
jeremymees <jeremymees123@gmail.com>
2023-09-27 12:49:40 +0200

    DND-329 homebrew link can be added and updated

--- components/molecules/HomebrewModal.vue
+++ components/molecules/HomebrewModal.vue
@@ -5,13 +5,14 @@ import logRocket from 'logrocket'
 const emit = defineEmits(['updated'])
 const props = withDefaults(
   defineProps<{
-    item?: Homebrew,
-    encounter?: boolean,
+    item?: Homebrew
+    encounter?: boolean
     update?: boolean
-  }>(), {
+  }>(),
+  {
     item: undefined,
     encounter: false,
-    update: false
+    update: false,
   }
 )
 
@@ -26,24 +27,30 @@ const form = ref<HomebrewForm>({
   name: props.item?.name || '',
   player: props.item?.player || '',
   link: props.item?.link || '',
-  type: props.item?.type || 'player' as RowType,
+  type: props.item?.type || ('player' as RowType),
   initiative: undefined,
   amount: undefined,
   summoner: undefined,
   ac: undefined,
   health: undefined,
-  actions: getActions()
+  actions: getActions(),
 })
 
-const dice = '<svg xmlns="http://www.w3.org/2000/svg" width="480" height="512" viewBox="0 0 480 512"><path fill="currentColor" d="M106.75 215.06L1.2 370.95c-3.08 5 .1 11.5 5.93 12.14l208.26 22.07l-108.64-190.1zM7.41 315.43L82.7 193.08L6.06 147.1c-2.67-1.6-6.06.32-6.06 3.43v162.81c0 4.03 5.29 5.53 7.41 2.09zM18.25 423.6l194.4 87.66c5.3 2.45 11.35-1.43 11.35-7.26v-65.67l-203.55-22.3c-4.45-.5-6.23 5.59-2.2 7.57zm81.22-257.78L179.4 22.88c4.34-7.06-3.59-15.25-10.78-11.14L17.81 110.35c-2.47 1.62-2.39 5.26.13 6.78l81.53 48.69zM240 176h109.21L253.63 7.62C250.5 2.54 245.25 0 240 0s-10.5 2.54-13.63 7.62L130.79 176H240zm233.94-28.9l-76.64 45.99l75.29 122.35c2.11 3.44 7.41 1.94 7.41-2.1V150.53c0-3.11-3.39-5.03-6.06-3.43zm-93.41 18.72l81.53-48.7c2.53-1.52 2.6-5.16.13-6.78l-150.81-98.6c-7.19-4.11-15.12 4.08-10.78 11.14l79.93 142.94zm79.02 250.21L256 438.32v65.67c0 5.84 6.05 9.71 11.35 7.26l194.4-87.66c4.03-1.97 2.25-8.06-2.2-7.56zm-86.3-200.97l-108.63 190.1l208.26-22.07c5.83-.65 9.01-7.14 5.93-12.14L373.25 215.06zM240 208H139.57L240 383.75L340.43 208H240z"/></svg>'
+const dice =
+  '<svg xmlns="http://www.w3.org/2000/svg" width="480" height="512" viewBox="0 0 480 512"><path fill="currentColor" d="M106.75 215.06L1.2 370.95c-3.08 5 .1 11.5 5.93 12.14l208.26 22.07l-108.64-190.1zM7.41 315.43L82.7 193.08L6.06 147.1c-2.67-1.6-6.06.32-6.06 3.43v162.81c0 4.03 5.29 5.53 7.41 2.09zM18.25 423.6l194.4 87.66c5.3 2.45 11.35-1.43 11.35-7.26v-65.67l-203.55-22.3c-4.45-.5-6.23 5.59-2.2 7.57zm81.22-257.78L179.4 22.88c4.34-7.06-3.59-15.25-10.78-11.14L17.81 110.35c-2.47 1.62-2.39 5.26.13 6.78l81.53 48.69zM240 176h109.21L253.63 7.62C250.5 2.54 245.25 0 240 0s-10.5 2.54-13.63 7.62L130.79 176H240zm233.94-28.9l-76.64 45.99l75.29 122.35c2.11 3.44 7.41 1.94 7.41-2.1V150.53c0-3.11-3.39-5.03-6.06-3.43zm-93.41 18.72l81.53-48.7c2.53-1.52 2.6-5.16.13-6.78l-150.81-98.6c-7.19-4.11-15.12 4.08-10.78 11.14l79.93 142.94zm79.02 250.21L256 438.32v65.67c0 5.84 6.05 9.71 11.35 7.26l194.4-87.66c4.03-1.97 2.25-8.06-2.2-7.56zm-86.3-200.97l-108.63 190.1l208.26-22.07c5.83-.65 9.01-7.14 5.93-12.14L373.25 215.06zM240 208H139.57L240 383.75L340.43 208H240z"/></svg>'
 
-whenever(() => isOpen.value, () => { form.value.actions = getActions() })
+whenever(
+  () => isOpen.value,
+  () => {
+    form.value.actions = getActions()
+  }
+)
 
 const summonersOptions = computed<Option[]>(() => {
   if (table.encounter?.rows && props.encounter) {
     return table.encounter.rows
       .filter(r => r.type !== 'summon')
-      .map((o) => {
+      .map(o => {
         return { label: o.name, value: o.id }
       })
   } else {
@@ -51,18 +58,13 @@ const summonersOptions = computed<Option[]>(() => {
   }
 })
 
-function getActions (): Action[] {
+function getActions(): Action[] {
   return props.item
-    ? [
-        ...props.item.actions,
-        ...props.item.legendary_actions,
-        ...props.item.reactions,
-        ...props.item.special_abilities
-      ]
+    ? [...props.item.actions, ...props.item.legendary_actions, ...props.item.reactions, ...props.item.special_abilities]
     : []
 }
 
-function handleSumbit ({ __init, data, slots, ...formData }: Obj): void {
+function handleSumbit({ __init, data, slots, ...formData }: Obj): void {
   error.value = ''
   isLoading.value = true
 
@@ -76,7 +78,7 @@ function handleSumbit ({ __init, data, slots, ...formData }: Obj): void {
 
     if (actions.length) {
       actions.forEach((a: Action) => {
-        homebrewData[a.type || 'action'] = [...homebrewData[a.type || 'action'] || [], a]
+        homebrewData[a.type || 'action'] = [...(homebrewData[a.type || 'action'] || []), a]
       })
     }
 
@@ -98,7 +100,7 @@ function handleSumbit ({ __init, data, slots, ...formData }: Obj): void {
   }
 }
 
-async function updateHomebrew (formData : Obj): Promise<void> {
+async function updateHomebrew(formData: Obj): Promise<void> {
   if (!props.item?.id) {
     return
   }
@@ -109,16 +111,16 @@ async function updateHomebrew (formData : Obj): Promise<void> {
   emit('updated', updated)
 }
 
-async function addHomebrew (formData : Obj): Promise<void> {
+async function addHomebrew(formData: Obj): Promise<void> {
   await store.addHomebrew(
     useEmptyKeyRemover({
       ...formData,
-      campaign: store?.campaign?.id
+      campaign: store?.campaign?.id,
     }) as AddHomebrew
   )
 }
 
-async function addInitiative ({ amount, ...data }: Obj): Promise<void> {
+async function addInitiative({ amount, ...data }: Obj): Promise<void> {
   if (!table.encounter?.rows) {
     return
   }
@@ -139,11 +141,11 @@ async function addInitiative ({ amount, ...data }: Obj): Promise<void> {
   }
 
   await table.encounterUpdate({
-    rows: [...table.encounter.rows, ...rows]
+    rows: [...table.encounter.rows, ...rows],
   } as UpdateEncounter)
 }
 
-function closeModal (): void {
+function closeModal(): void {
   form.value.type = 'player'
   isOpen.value = false
 }
@@ -152,16 +154,8 @@ function closeModal (): void {
 <template>
   <section>
     <button
-      v-tippy="{ content: $t(
-        encounter
-          ? 'components.homebrewModal.add'
-          : update ? 'actions.update' : 'actions.add'
-      ) }"
-      :aria-label="$t(
-        encounter
-          ? 'components.homebrewModal.add'
-          : update ? 'actions.update' : 'actions.add'
-      )"
+      v-tippy="{ content: $t(encounter ? 'components.homebrewModal.add' : update ? 'actions.update' : 'actions.add') }"
+      :aria-label="$t(encounter ? 'components.homebrewModal.add' : update ? 'actions.update' : 'actions.add')"
       class="flex gap-2 items-center disabled:opacity-40 disabled:cursor-not-allowed"
       @click="isOpen = true"
     >
@@ -173,7 +167,7 @@ function closeModal (): void {
         :class="{
           'text-info w-6 h-6': update && !encounter,
           'text-success w-6 h-6': !update && !encounter,
-          'text-warning w-10 h-10': !update && encounter
+          'text-warning w-10 h-10': !update && encounter,
         }"
         aria-hidden="true"
       />
@@ -184,16 +178,10 @@ function closeModal (): void {
           {{ $t(`components.homebrewModal.${update ? 'update' : 'new'}`) }}
         </h2>
       </template>
-      <FormKit
-        id="form"
-        v-model="form"
-        type="form"
-        :actions="false"
-        @submit="handleSumbit"
-      >
+      <FormKit id="form" v-model="form" type="form" :actions="false" @submit="handleSumbit">
         <div
           :class="{
-            'grid sm:grid-cols-2 gap-x-3': (form.type === 'monster' || form.type === 'summon') && encounter
+            'grid sm:grid-cols-2 gap-x-3': (form.type === 'monster' || form.type === 'summon') && encounter,
           }"
         >
           <FormKit
@@ -207,7 +195,7 @@ function closeModal (): void {
               { label: 'Summon', value: 'summon' },
               { label: 'Npc', value: 'npc' },
               { label: 'Monster', value: 'monster' },
-              { label: 'Lair', value: 'lair' }
+              { label: 'Lair', value: 'lair' },
             ]"
             outer-class="grow"
           />
@@ -259,10 +247,7 @@ function closeModal (): void {
             form.initiative = useDiceRoll(20) as number
           }"
         />
-        <div
-          v-if="form.type !== 'lair'"
-          class="flex flex-row gap-x-3 items-center"
-        >
+        <div v-if="form.type !== 'lair'" class="flex flex-row gap-x-3 items-center">
           <FormKit
             name="ac"
             type="number"
@@ -282,6 +267,7 @@ function closeModal (): void {
             outer-class="grow"
           />
         </div>
+        <FormKit name="link" type="url" :label="$t('components.inputs.linkLabel')" validation="url" />
         <FormKit
           type="repeater"
           name="actions"
@@ -301,14 +287,10 @@ function closeModal (): void {
               { label: 'Action', value: 'actions' },
               { label: 'Legendary action', value: 'legendary_actions' },
               { label: 'Reaction', value: 'reactions' },
-              { label: 'Special ability', value: 'special_abilities' }
+              { label: 'Special ability', value: 'special_abilities' },
             ]"
           />
-          <FormKit
-            name="name"
-            :label="$t('components.inputs.nameLabel')"
-            validation="required|length:3,30"
-          />
+          <FormKit name="name" :label="$t('components.inputs.nameLabel')" validation="required|length:3,30" />
           <FormKit
             name="desc"
             type="textarea"
@@ -349,11 +331,13 @@ function closeModal (): void {
         <div class="flex justify-end">
           <FormKit
             type="submit"
-            :label="$t(
-              encounter || (!encounter && !update)
-                ? 'components.homebrewModal.add'
-                :'components.homebrewModal.update'
-            )"
+            :label="
+              $t(
+                encounter || (!encounter && !update)
+                  ? 'components.homebrewModal.add'
+                  : 'components.homebrewModal.update'
+              )
+            "
             :disabled="isLoading"
             input-class="$reset btn-primary"
           />
