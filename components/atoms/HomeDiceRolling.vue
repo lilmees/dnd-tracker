8e2ad0b3f70898a1e2b8d60770a8b189ff6356aa
jeremymees <jeremymees123@gmail.com>
2023-10-09 08:28:44 +0200

    DND-353 home dice rolling update

--- components/atoms/HomeDiceRolling.vue
+++ components/atoms/HomeDiceRolling.vue
@@ -43,15 +43,15 @@ function rollDice (dice: string): void {
       <template v-for="dice in ['d100', 'd20', 'd12', 'd10', 'd8', 'd6', 'd4']" :key="dice">
         <button
           v-tippy="{content: dice.toUpperCase(), placement: 'bottom'}"
-          class="bg-black rounded-lg p-2 border-4 w-14 h-14"
+          class="rounded-lg p-2 border-4 w-14 h-14"
           :class="{
-            'border-secondary': dice === 'd100',
-            'border-primary': dice === 'd20',
-            'border-warning': dice === 'd12',
-            'border-info': dice === 'd10',
-            'border-danger': dice === 'd8',
-            'border-help': dice === 'd6',
-            'border-success': dice === 'd4',
+            'border-secondary bg-secondary/50': dice === 'd100',
+            'border-primary bg-primary/50': dice === 'd20',
+            'border-warning bg-warning/50': dice === 'd12',
+            'border-info bg-info/50': dice === 'd10',
+            'border-danger bg-danger/50': dice === 'd8',
+            'border-help bg-help/50': dice === 'd6',
+            'border-success bg-success/50': dice === 'd4',
           }"
           @click="rollDice(dice)"
         >
@@ -78,13 +78,13 @@ function rollDice (dice: string): void {
         v-if="rolled && !ready"
         class="bg-black border-4 rounded-lg py-2 px-6 w-fit mt-4"
         :class="{
-          'border-secondary': rolled.dice === 'd100',
-          'border-primary': rolled.dice === 'd20',
-          'border-warning': rolled.dice === 'd12',
-          'border-info': rolled.dice === 'd10',
-          'border-danger': rolled.dice === 'd8',
-          'border-help': rolled.dice === 'd6',
-          'border-success': rolled.dice === 'd4',
+          'border-secondary bg-secondary/50': rolled.dice === 'd100',
+          'border-primary bg-primary/50': rolled.dice === 'd20',
+          'border-warning bg-warning/50': rolled.dice === 'd12',
+          'border-info bg-info/50': rolled.dice === 'd10',
+          'border-danger bg-danger/50': rolled.dice === 'd8',
+          'border-help bg-help/50': rolled.dice === 'd6',
+          'border-success bg-success/50': rolled.dice === 'd4',
         }"
       >
         <h3 class="text-center">
