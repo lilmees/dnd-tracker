c305dfc1d24f8ca046b7642aae5274093b67e66c
jeremymees <jeremymees123@gmail.com>
2023-10-14 15:04:13 +0200

    DND-374 mac shortcuts

--- components/atoms/Hotkeys.vue
+++ components/atoms/Hotkeys.vue
@@ -16,13 +16,19 @@
         <p class="head-3 text-center pb-2">
           Hotkeys
         </p>
-        <div class="grid grid-cols-2 gap-y-4">
+        <div class="grid grid-cols-3 gap-y-4">
           <p class="font-bold">
             {{ $t('actions.prev') }}
           </p>
-          <div class="flex flex-col gap-2">
+          <div class="flex flex-col gap-2 col-span-2">
             <div>
-              <kbd>Shift</kbd>+<kbd>ArrowLeft</kbd>
+              <kbd><Icon name="vaadin:ctrl-a" class="w-5 h-5" /></kbd>
+              <span class="mx-1 font-bold">/</span>
+              <kbd><Icon name="carbon:mac-command" class="w-5 h-5" /></kbd>
+              <span class="mx-1 font-bold">/</span>
+              <kbd><Icon name="carbon:mac-shift" class="w-5 h-5" /></kbd>
+              <span class="mx-1 font-bold">+</span>
+              <kbd><Icon name="carbon:arrow-left" class="w-5 h-5" /></kbd>
             </div>
             <div>
               {{ $t('general.or') }} <kbd>PageUp</kbd>
@@ -31,9 +37,15 @@
           <p class="font-bold">
             {{ $t('actions.next') }}
           </p>
-          <div class="flex flex-col gap-2">
+          <div class="flex flex-col gap-2 col-span-2">
             <div>
-              <kbd>Shift</kbd>+<kbd>ArrowRight</kbd>
+              <kbd><Icon name="vaadin:ctrl-a" class="w-5 h-5" /></kbd>
+              <span class="mx-1 font-bold">/</span>
+              <kbd><Icon name="carbon:mac-command" class="w-5 h-5" /></kbd>
+              <span class="mx-1 font-bold">/</span>
+              <kbd><Icon name="carbon:mac-shift" class="w-5 h-5" /></kbd>
+              <span class="mx-1 font-bold">+</span>
+              <kbd><Icon name="carbon:arrow-right" class="w-5 h-5" /></kbd>
             </div>
             <div>
               {{ $t('general.or') }} <kbd>PageDown</kbd>
