2216cd122ee37b29128edb699e127a2e0d29122d
jeremymees <jeremymees123@gmail.com>
2024-01-28 17:15:34 +0100

    DND-471 updated homepage styling

--- components/atoms/Summary.vue
+++ components/atoms/Summary.vue
@@ -1,25 +1,36 @@
 <script setup lang="ts">
 withDefaults(
-  defineProps<{ title?: string, items?: string[]}>(),
-  { title: '', items: () => [] }
+  defineProps<{
+    title?: string
+    items?: string[]
+    sprite?: string
+  }>(),
+  {
+    title: '',
+    items: () => [],
+    sprite: undefined
+  }
 )
 </script>
 
 <template>
-  <div>
-    <h2 v-if="title" class="pb-4">
+  <div class="group">
+    <h2 v-if="title" class="pb-4 relative max-w-max">
       {{ title }}
+      <img
+        v-if="sprite"
+        :src="`/art/${sprite}`"
+        loading="lazy"
+        class="w-10 h-10 absolute -right-12 -top-3 z-[-1] group-hover:rotate-12 transition-transform group-hover:scale-[1.2]"
+      >
     </h2>
     <div v-if="items.length" class="flex flex-col gap-4">
       <div v-for="item in items" :key="item" class="flex items-center gap-4">
-        <NuxtImg
-          src="/dice.webp"
-          alt="Dice"
-          sizes="sm:40px md:40px lg:40px"
-          format="webp"
-          provider="imagekit"
-          class="w-6 h-6 animate-pulse"
-        />
+        <img
+          src="/art/items/coin.svg"
+          loading="lazy"
+          class="w-6 h-6"
+        >
         <p>{{ item }}</p>
       </div>
     </div>
--- components/atoms/TitleText.vue
+++ components/atoms/TitleText.vue
@@ -1,27 +1,39 @@
 <script setup lang="ts">
 withDefaults(
   defineProps<{
-  title?: string,
-  text?: string,
-  text2?: string,
-  buttonLabel?: string,
-  buttonLink?: string,
+  title?: string
+  text?: string
+  text2?: string
+  buttonLabel?: string
+  buttonLink?: string
   center?: boolean
+  sprite?: string
   }>(), {
     text: '',
     text2: '',
     title: '',
     center: false,
     buttonLabel: '',
-    buttonLink: ''
+    buttonLink: '',
+    sprite: undefined
   }
 )
 </script>
 
 <template>
-  <div :class="{ 'text-center': center }">
-    <h2 v-if="title" class="pb-4">
+  <div class="group" :class="{ 'text-center': center }">
+    <h2
+      v-if="title"
+      class="pb-4 relative max-w-max"
+      :class="{ 'mx-auto': center }"
+    >
       {{ title }}
+      <img
+        v-if="sprite"
+        :src="`/art/${sprite}`"
+        loading="lazy"
+        class="w-10 h-10 absolute -right-12 -top-3 z-[-1] group-hover:rotate-12 transition-transform group-hover:scale-[1.2]"
+      >
     </h2>
     <p
       v-if="text"
