586ea780a635d030cade5924def51c4b46a83318
jeremymees <jeremymees123@gmail.com>
2024-03-26 16:53:55 +0100

    DND-507 updated styling of tag and added level selector for when condition levels has

--- components/skeletons/SkeletonTag.vue
+++ components/skeletons/SkeletonTag.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="px-3 py-1 m-1 rounded-lg bg-danger/50 w-fit animate-pulse flex gap-x-2">
-    <div class="w-6 h-6 rounded-full bg-danger" />
-    <div class="w-20 h-6 rounded-full bg-danger" />
-    <div class="w-6 h-6 rounded-full bg-danger" />
+  <div class="px-2 py-[5px] rounded-lg bg-black/50 w-fit animate-pulse flex gap-x-2 border-4 border-black">
+    <div class="w-20 h-6 rounded-full bg-black" />
+    <div class="w-6 h-6 rounded-full bg-black" />
   </div>
 </template>
