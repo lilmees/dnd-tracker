b300e20e73ef23d80dcc061680cbab1b74434a24
jeremymees <jeremymees123@gmail.com>
2024-04-12 21:23:40 +0200

    DND-538 error when refreshing on campaign page

--- pages/campaigns/[id]-[title]/content.vue
+++ pages/campaigns/[id]-[title]/content.vue
@@ -3,6 +3,8 @@
     <Encounters campaign-view />
     <NuxtLazyHydrate when-visible>
       <Homebrews class="py-10" />
+    </NuxtLazyHydrate>
+    <NuxtLazyHydrate when-visible>
       <Notes />
     </NuxtLazyHydrate>
   </div>
